1ccb5367b86c9d990d615512e61610f335067cd0
Meriç <meric.balikci@gmail.com>
2020-02-03 17:11:10 +0300

    Day 01 has been added.

---
+++ 2019/Day 01/Fuel_Management_System.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Fuel_Management_System
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    private static List<int> positions;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static List<int> Positions
+    {
+        get { return positions; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="positionData"></param>
+    /// <returns></returns>
+    public static int CalculateFuelNeeded(int positionData)
+    {
+        return (int) Math.Floor(positionData / 3f) - 2;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="positionDatas"></param>
+    /// <returns></returns>
+    public static int TotalFuelNeeded(List<int> positionDatas)
+    {
+        int totalFuel = 0;
+        foreach (int positionData in positionDatas)
+        {
+            int result = 0;
+            result = CalculateFuelNeeded(positionData);
+            totalFuel += result;
+            while (result > 0)
+            {
+                result = CalculateFuelNeeded(result);
+                if (result > 0)
+                {
+                    totalFuel += result;
+                    Console.WriteLine(result);
+                }
+            }
+        }
+        return totalFuel;
+    }
+}
---
+++ 2019/Day 01/Solution_01.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Solution_01
+{
+    public void TakeInput()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SolvePartOne()
+    {
+        int result = 0;
+        foreach (int position in Fuel_Management_System.Positions)
+        {
+            result += Fuel_Management_System.CalculateFuelNeeded(position);
+        }
+        Console.WriteLine(result.ToString());
+    }
+
+    public void SolvePartTwo()
+    {
+        Console.WriteLine(Fuel_Management_System.TotalFuelNeeded(Fuel_Management_System.Positions));
+    }
+}
