4fee1262d3658e6489d0c0b1b02450017f9203e4
Meriç <meric.balikci@gmail.com>
2020-02-03 18:41:34 +0300

    Day 02 has been added.

---
+++ 2019/Day 02/IntCode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// 
+/// </summary>
+public static class IntCode
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public static int CalculateOpcode(List<int> IntCodeInput)
+    {
+        int index = 0;
+        while (true)
+        {
+            if (IntCodeInput[index] == 99)
+            {
+                return IntCodeInput[0];
+            }
+
+            int value1 = IntCodeInput[IntCodeInput[index + 1]];
+            int value2 = IntCodeInput[IntCodeInput[index + 2]];
+            switch (IntCodeInput[index])
+            {
+                case 1:
+                    IntCodeInput[IntCodeInput[index + 3]] = value1 + value2;
+                    index += 4;
+                    break;
+                case 2:
+                    IntCodeInput[IntCodeInput[index + 3]] = value1 * value2;
+                    index += 4;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ 2019/Day 02/Solution_02.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public static class Solution_02
+{
+    private static List<int> input = new List<int>();
+
+    private static void TakeInput()
+    {
+       List<string> rawInput = File.ReadAllText(Program.DefaultInputPath+"/Day 02.txt").Split(",").ToList();
+       foreach (string str in rawInput)
+       {
+           input.Add(Int32.Parse(str));
+       }
+    }
+
+    public static void SolvePartOne()
+    {
+        TakeInput();
+        input[1] = 12;
+        input[2] = 2;
+        Console.WriteLine(IntCode.CalculateOpcode(input));
+    }
+
+    public static void SolvePartTwo()
+    {
+        for (int a = 0; a < 99; a++)
+        {
+            for (int b = 0; b < 99; b++)
+            {
+                input = new List<int>();
+                TakeInput();
+                input[1] = a;
+                input[2] = b;
+                if (IntCode.CalculateOpcode(input) != 19690720) continue;
+                Console.WriteLine("a: " + a + " b: " +b);
+                Console.WriteLine((100*a)+b);
+                return;
+            }
+        }
+    }
+}
